74fce0424c2a80da69ca654b49ba32ab22c4ffdb
daxyonis <emaciejko@gmail.com>
2016-09-30 21:18:21 -0400

    Removing slash in view name not login page

--- src/main/java/com/emaciejko/config/WebConfig.java
+++ src/main/java/com/emaciejko/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("/view/login");
+        registry.addViewController("/login").setViewName("view/login");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 }
