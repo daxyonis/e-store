9ad290d8aeca4b545610c02b82feb735fb5fffee
daxyonis <emaciejko@gmail.com>
2016-10-07 15:00:41 -0400

    Add static initialization otherwise naming map is empty at startup

--- src/main/java/com/emaciejko/domain/Province.java
+++ src/main/java/com/emaciejko/domain/Province.java
@@ -8,8 +8,19 @@ import com.emaciejko.util.I18n;
 public enum Province {
     BC, AB, SK, MB, ON, QC, NB, NS, PE, NL; 
     
-    private static Map<Province, String> mappedNames = new LinkedHashMap<>();    
+    private static Map<Province, String> mappedNames  = new LinkedHashMap<>();
     
+    /**
+     * static initialization
+     */
+    static
+    {	
+	mapNames();
+    }
+    
+    /**
+     *  (Re)mapping of the province names
+     */
     private static void mapNames() {
 	mappedNames.clear();
 	for(Province p : Province.values()){
@@ -17,15 +28,28 @@ public enum Province {
 	    }
     }
     
+    /**
+     * reset the province naming map
+     * useful after programmatically changing the locale 
+     */
     public static void reset(){
 	I18n.reset();
 	mapNames();
     }
     
+    /**
+     * Static map accessor
+     * @return  the map of province enums with the translated province names as values
+     * Depends on the locale as given by I18n
+     */
     public static Map<Province, String> getKeyedNames() {
 	return mappedNames;
 	}
     
+    /**
+     * 
+     * @return  this province's name (locale dependent)
+     */
     public String getName(){	
 	return mappedNames.get(this);
     }
